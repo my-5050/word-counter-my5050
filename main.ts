af199eca29f2f1c8395e6f2a81f526ad8997ee80
my-5050 <165835048+my-5050@users.noreply.github.com>
2024-05-08 12:38:14 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// declare a constant answer and assign it to the result of inquirer.prompt function..
+
+const answer:{
+    sentence:string
+}= await inquirer.prompt({
+    name:"sentence",
+    type:"input",
+    message:"enter your sentence to count the words: "
+
+});
+const words =answer.sentence.trim().split(" ")
+
+// print the array words to the concole
+console.log(words)
+
+// print the words count of the sentence to the console.
+
+console.log(`your sentence words count is ${words.length}`);
